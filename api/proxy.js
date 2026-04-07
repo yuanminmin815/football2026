fcfe8034cd1258c0944e20051b41e970e3e86c2b
yuanminmin815 <xiaorenyuan0@gmail.com>
2026-04-07 13:24:57 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,11 +1,5 @@
 export default async function handler() {
-  return new Response(JSON.stringify({
-    league:"欧冠",
-    updateTime:new Date().toISOString()
-  }),{
-    headers:{
-      "Access-Control-Allow-Origin":"*",
-      "Cache-Control":"no-cache"
-    }
+  return new Response(JSON.stringify({ league: "欧冠", update: new Date().toISOString() }), {
+    headers: { "Access-Control-Allow-Origin": "*", "Cache-Control": "no-cache" }
   });
 }
